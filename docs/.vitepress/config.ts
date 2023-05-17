597115eca7ccbbbd4288a6ae73404f4e9cde7290
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-17 15:36:34 +0800

    chore(vitepress): 🔧 set base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/mcu-extra/',
   description: 'Additional packages to add new features and bug fixes to Material Color Utilities.',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
